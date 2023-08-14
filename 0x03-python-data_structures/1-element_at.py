ead6e643fbac78079631267787f14f809dab6319
KelvinoKing <kelvinoking@outlook.com>
2023-08-14 09:10:48 +0000

    Retrives an element from a list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    list_len = len(my_list)
+    if idx >= list_len:
+        return None
+    ele = my_list.pop(idx)
+    return ele
