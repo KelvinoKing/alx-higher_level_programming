2a54a110df50557ebf19731be364bebebb30b69f
KelvinoKing <kelvinoking@outlook.com>
2023-08-31 09:43:24 +0000

    Implements getters and setters

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+Square class
+"""
+
+
+class Square:
+    """The class uses setters and getter methods to set and retrive the size"""
+
+    def __init__(self, size):
+        """__init__ method to initialize value
+
+        args:
+            value (int): value of square
+
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """ size method returns the value of the square
+
+        size(value) sets the value of square
+
+        args:
+            value (int): for the setter
+        Returns: value of square
+
+        Raises:
+            TypeError: if value is not int
+            ValueError: if value is < 0
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """area method calculates the area of square
+
+        Returns: area of square
+        """
+
+        return self.__size * self.__size
