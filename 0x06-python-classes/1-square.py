5ccf83960613c95b2ee3e97ae63951c0f905b26e
KelvinoKing <kelvinoking@outlook.com>
2023-08-31 08:14:46 +0000

    Class with attribute size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+class Square():
+    """Defines a square by attribute of size."""
+
+    def __init__(self, size):
+        """Creates a class with a specified size.
+
+        Args:
+            size: Initializes the size of the square
+
+        """
+        self.__size = size
