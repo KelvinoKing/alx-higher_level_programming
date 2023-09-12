a0b69c39061556e8daed9d7e8f349f2d28b3d14c
KelvinoKing <kelvinoking@outlook.com>
2023-09-12 13:14:45 +0000

    BaseGeometry class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""BaseGeometry module
+It is a class
+"""
+
+
+class BaseGeometry(object):
+    """BaseGeometry class
+    """
+    def __init__(self):
+        """class function
+        constructor
+        """
+        pass
+
+    def area(self):
+        """area function
+        not implemented
+        """
+        raise Exception('area() is not implemented')
