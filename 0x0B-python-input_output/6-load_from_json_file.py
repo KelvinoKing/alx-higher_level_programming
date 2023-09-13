36054db76ab5a743deb164934fd0b1411b0d24f9
KelvinoKing <kelvinoking@outlook.com>
2023-09-13 18:46:20 +0000

    Creates an Object from a JSON file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""json module
+deserialize an object
+"""
+
+import json
+
+"""load_from_json_file
+creates an Object from a JSON file
+"""
+
+
+def load_from_json_file(filename):
+    """creates an Object from a JSON file
+
+    args:
+        filename: name of JSON file
+    """
+
+    with open(filename, encoding="utf-8") as my_file:
+        json.load(my_file)
