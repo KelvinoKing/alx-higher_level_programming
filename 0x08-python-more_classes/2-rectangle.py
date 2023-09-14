cb3da30358bf38665d0a3b3821e2e4d1624d43fb
KelvinoKing <kelvinoking@outlook.com>
2023-09-14 11:43:54 +0000

    Gets area and parameter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -49,4 +49,4 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return 0
         else:
-            return (self.height + self.width) * 2
+            return self.height * 2 + self.width * 2
