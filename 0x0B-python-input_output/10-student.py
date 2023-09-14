01921ae74c4442107a988390011a6b436385250a
KelvinoKing <kelvinoking@outlook.com>
2023-09-14 10:20:05 +0000

    class Student that defines a student by: (based on 9-student.py)

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Student class
+"""
+
+
+class Student(object):
+    """Student class
+    defines a student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """__init__
+        class constructor with specified attributes
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """to_json method
+        returns a dictionary rep of class attributes
+        """
+
+        obj_dict = self.__dict__
+        if not attrs:
+            return obj_dict
+        else:
+            filterd_dict = {}
+            for att in attrs:
+                if hasattr(self, att):
+                    filterd_dict[att] = obj_dict[att]
+            return filterd_dict
