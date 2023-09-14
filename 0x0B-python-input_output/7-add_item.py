27bfb4a332912f80b448f404191c0583ee52ce45
KelvinoKing <kelvinoking@outlook.com>
2023-09-14 09:40:43 +0000

    adds all arguments to a Python list, and then save them to a file:

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""sys module
+allow access to the OS
+
+json module
+serialize and deserialize objects
+"""
+
+import sys
+import json
+import os
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+filename = 'add_item.json'
+args = len(sys.argv)
+
+if not os.path.isfile(filename):
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write('[]')
+
+
+if args > 1:
+    data = load_from_json_file(filename)
+    for i in range(1, args):
+        data.append(sys.argv[i])
+    save_to_json_file(data, filename)
