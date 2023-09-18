cfd1448f934b7a8151ec4250a240e8aef569ee14
KelvinoKing <kelvinoking@outlook.com>
2023-09-18 07:30:18 +0000

    Tests update

---
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+"""tests for the Rectangle class
+using unittest
+"""
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+
+
+class TestRectangleClass(unittest.TestCase):
+    """class contains tests for the rectangle class
+    """
+
+    def test_instance(self):
+        """Rectange class unittest"""
+        r5 = Rectangle(10, 2)
+        self.assertIsInstance(r5, Base)
+        self.assertIsInstance(r5, Rectangle)
+
+    def test_with_arguments(self):
+        """Rectangle test with args and without"""
+        r6 = Rectangle(3, 5)
+        r7 = Rectangle(10, 2, 0, 0, 12)
+        self.assertEqual(r6.id, 7)
+        self.assertEqual(r7.id, 12)
+        self.assertEqual(r6.x, 0)
+        self.assertEqual(r6.y, 0)
+
+    def test_with_attributes(self):
+        """test Rectangle class using wrong
+        data types
+        """
+        r8 = Rectangle(10, 2)
+        with self.assertRaises(AttributeError):
+            r8.__width
+        with self.assertRaises(AttributeError):
+            r8.__height
+        with self.assertRaises(AttributeError):
+            r8.__x
+        with self.assertRaises(AttributeError):
+            r8.__y
+
+    def test_attr_with_wrong_types(self):
+        """test Rectangle class using wrong values
+        """
+        with self.assertRaises(TypeError):
+            r9 = Rectangle('a', 2)
+        with self.assertRaises(TypeError):
+            r11 = Rectangle(2, 'a')
+        with self.assertRaises(TypeError):
+            r12 = Rectangle(2, 2, 'a')
+        with self.assertRaises(TypeError):
+            r13 = Rectangle(2, 2, 2, 'a')
+
+    def test_attr_with_wrong_range(self):
+        """test the attributes for wrong ranges
+        """
+        with self.assertRaises(ValueError):
+            r14 = Rectangle(0, 2)
+        with self.assertRaises(ValueError):
+            r15 = Rectangle(2, 0)
+        with self.assertRaises(ValueError):
+            r16 = Rectangle(-2, 2)
+        with self.assertRaises(ValueError):
+            r17 = Rectangle(2, -2)
+        with self.assertRaises(ValueError):
+            r18 = Rectangle(2, 2, -1)
+        with self.assertRaises(ValueError):
+            r19 = Rectangle(2, 2, 0, -1)
+
+    def test_area_method(self):
+        """Tets for the area method in Rectangle
+        """
+        r20 = Rectangle(10, 10)
+        self.assertEqual(r20.area(), 100)
