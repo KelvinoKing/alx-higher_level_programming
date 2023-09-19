04641cff27dc879eaad73e95bd3c87e373c35240
KelvinoKing <kelvinoking@outlook.com>
2023-09-19 09:08:31 +0000

    updates

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -50,3 +50,12 @@ class Base(object):
 
         with open(cls.__name__ + ".json", 'w') as f:
             f.write(obj_string)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON
+        sring representation json_string
+        """
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.load(json_string)
