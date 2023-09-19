30c4f7ccc44b3ce6afaa518e83006fa6cfddc450
KelvinoKing <kelvinoking@outlook.com>
2023-09-19 08:48:26 +0000

    updates

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,8 +26,6 @@ class Base(object):
         """returns a JSON string representation of
         list_dictionaries
         """
-        if list_dictionaries is None:
-            return "[]"
-        if len(list_dictionaries) == 0:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
