9b7036c084946d697426c028510ad5ee672927f1
KelvinoKing <kelvinoking@outlook.com>
2023-09-19 08:51:05 +0000

    updates

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,10 +22,13 @@ class Base(object):
             Base.__no_objects += 1
             self.id = Base.__no_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """returns a JSON string representation of
         list_dictionaries
         """
-        if list_dictionaries is None or len(list_dictionaries) == 0:
+        if list_dictionaries is None:
+            return "[]"
+        if len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
