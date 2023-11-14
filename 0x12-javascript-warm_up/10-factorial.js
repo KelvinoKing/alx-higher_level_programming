d205d1de240f5408794ae1fb43c7f8bd661a9b82
KelvinoKing <kelvinoking@outlook.com>
2023-11-14 13:57:26 +0000

    Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,12 +2,12 @@
 const args = process.argv.slice(2);
 const myInt = parseInt(args[0]);
 
-const factorial = (n) => {
+function factorial (n) {
   if (n === 0 || n === 1) {
     return 1;
   }
   return n * factorial(n - 1);
-};
+}
 
 if (!myInt) {
   console.log(1);
