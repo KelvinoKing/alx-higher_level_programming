dc5fbb962a8d7eb27a2fea8430a9282d740d5e13
KelvinoKing <kelvinoking@outlook.com>
2023-11-14 14:28:25 +0000

    Second biggest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+const lenArgs = args.length;
+
+function largest (n) {
+  let temp = n[0];
+  let secLargest = n[0];
+
+  for (let i = 0; i < n.length; i++) {
+    if (n[i] > temp) {
+      secLargest = temp;
+      temp = n[i];
+    }
+  }
+  return secLargest;
+}
+
+if (lenArgs === 0 || lenArgs === 1) {
+  console.log(0);
+} else {
+  const newArray = [];
+
+  for (let i = 0; i < lenArgs; i++) {
+    newArray.push(parseInt(args[i]));
+  }
+  console.log(largest(newArray));
+}
