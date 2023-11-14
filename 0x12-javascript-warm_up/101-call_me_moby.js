43a44c4d4d67e6faf331a9d59611c1ab97afcc9a
KelvinoKing <kelvinoking@outlook.com>
2023-11-14 17:11:44 +0000

    Call me Moby

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+function callMeMoby (x, theFunction) {
+  if (x > 0) {
+    theFunction();
+    exports.callMeMoby(x - 1, theFunction);
+  }
+}
+
+module.exports = {
+  callMeMoby
+};
