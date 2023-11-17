209d498a62e0c56c794a6a956f9f6b248ea722b3
KelvinoKing <kelvinoking@outlook.com>
2023-11-17 08:33:44 +0000

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,24 +8,26 @@ if __name__ == "__main__":
     cur = None
     db = None
 
-    if len(sys.argv) != 4:
-        print("Requires 4 arguments")
-        exit(0)
-    else:
-        try:
-            db = MySQLdb.connect(
-                    host=sys.argv[1], port=3306, user=sys.argv[2],
-                    passwd=sys.argv[3], db="hbtn_0e_0_usa")
+    try:
+        if len(sys.argv) != 4:
+            print("Requires 4 arguments")
+            exit(0)
 
-            cur = db.cursor()
-            cur.execute("SELECT * FROM states ORDER BY `id` ASC")
-            allInfo = cur.fetchall()
-            for info in allInfo:
-                print(info)
-        except MySQLdb.Error as e:
-            print("MySQL Error[%d]: %s" % (e.args[0], e.args[1]))
-        finally:
-            if cur is not None:
-                cur.close()
-            if db is not None:
-                db.close()
+        db = MySQLdb.connect(
+                host=sys.argv[1], port=3306, user=sys.argv[2],
+                passwd=sys.argv[3], db="hbtn_0e_0_usa")
+
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states ORDER BY `id` ASC")
+        allInfo = cur.fetchall()
+        for info in allInfo:
+            print(info)
+
+    except MySQLdb.Error as e:
+        print("MySQL Error[%d]: %s" % (e.args[0], e.args[1]))
+
+    finally:
+        if cur is not None:
+            cur.close()
+        if db is not None:
+            db.close()
