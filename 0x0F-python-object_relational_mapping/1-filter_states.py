fb1acea29c6d9da217e8782aec727de3db7c75ea
KelvinoKing <kelvinoking@outlook.com>
2023-11-17 09:09:32 +0000

    Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""MySQLdb module
+"""
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    try:
+        if len(sys.argv) != 4:
+            print("Requires 4 arguments")
+            exit(0)
+
+        username, password, database = sys.argv[1:4]
+
+        with MySQLdb.connect(
+                host="localhost", port=3306, user=username,
+                passwd=password, db=database) as db:
+
+            with db.cursor() as cur:
+                cur.execute(
+                        "SELECT * FROM states WHERE `name` LIKE 'N%' \
+                                ORDER BY `id` ASC")
+                allInfo = cur.fetchall()
+                for info in allInfo:
+                    print(info)
+
+    except MySQLdb.Error as e:
+        print("MySQL Error[%d]: %s" % (e.args[0], e.args[1]))
+    except Exception as e:
+        print("An error occurred:", e)
