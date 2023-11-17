2e894e4731b61175959655ebd576a9e6674f8d46
KelvinoKing <kelvinoking@outlook.com>
2023-11-17 08:12:25 +0000

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
 """MySQLdb module
 """
-import MySQLdb
-import sys
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
 
-if len(sys.argv) != 4:
-    print("Requires 4 arguments")
-    exit(0)
-else:
-    try:
-        db = MySQLdb.connect(
-                host=sys.argv[1], port=3306, user=sys.argv[2],
-                passwd=sys.argv[3], db="hbtn_0e_0_usa")
+    if len(sys.argv) != 4:
+        print("Requires 4 arguments")
+        exit(0)
+    else:
+        try:
+            db = MySQLdb.connect(
+                    host=sys.argv[1], port=3306, user=sys.argv[2],
+                    passwd=sys.argv[3], db="hbtn_0e_0_usa")
 
-        cur = db.cursor()
-        cur.execute("SELECT * FROM states ORDER BY `id` ASC")
-        allInfo = cur.fetchall()
-        for info in allInfo:
-            print(info)
-    except MySQLdb.Error as e:
-        print("MySQL Error[%d]: %s" % (e.args[0], e.args[1]))
-    finally:
-        cur.close()
-        db.close()
+            cur = db.cursor()
+            cur.execute("SELECT * FROM states ORDER BY `id` ASC")
+            allInfo = cur.fetchall()
+            for info in allInfo:
+                print(info)
+        except MySQLdb.Error as e:
+            print("MySQL Error[%d]: %s" % (e.args[0], e.args[1]))
+        finally:
+            cur.close()
+            db.close()
