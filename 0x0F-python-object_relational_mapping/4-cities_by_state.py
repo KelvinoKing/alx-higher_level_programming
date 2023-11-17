103a8ef44a2059de40e8926f608232233646e2f2
KelvinoKing <kelvinoking@outlook.com>
2023-11-17 11:33:02 +0000

    Cities by state

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""MySQLdb module
+"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+    import re
+
+    db = None
+    cur = None
+
+    try:
+        if len(sys.argv) != 4:
+            print("Requires 4 arguments")
+            sys.exit(0)
+
+        username, password, database = sys.argv[1:4]
+
+        if not re.match(r'^[a-zA-Z0-9_]*$', database):
+            raise ValueError("Invalid database name")
+
+        with MySQLdb.connect(
+                host="localhost", port=3306, user=username,
+                passwd=password, db=database) as db:
+
+            with db.cursor() as cur:
+                query = "SELECT \
+                        cities.id, cities.name, states.name \
+                        FROM `{}`.cities JOIN `hbtn_0e_0_usa`.states \
+                        ON cities.state_id=states.id;".format(database)
+                cur.execute(query)
+                AllInfo = cur.fetchall()
+                for info in AllInfo:
+                    print(info)
+
+    except MySQLdb.Error as e:
+        print("MySQLdb Error[%d]: %s" % (e.args[0], e.args[1]))
+    except Exception as e:
+        print("An error occured:", e)
