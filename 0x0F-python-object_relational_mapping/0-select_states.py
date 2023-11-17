7f919bde2bba93f28023229d0031dae966fc21c4
KelvinoKing <kelvinoking@outlook.com>
2023-11-17 08:43:20 +0000

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,29 +5,22 @@ if __name__ == "__main__":
     import MySQLdb
     import sys
 
-    cur = None
-    db = None
-
     try:
         if len(sys.argv) != 4:
             print("Requires 4 arguments")
             exit(0)
 
-        db = MySQLdb.connect(
+        with MySQLdb.connect(
                 host=sys.argv[1], port=3306, user=sys.argv[2],
-                passwd=sys.argv[3], db="hbtn_0e_0_usa")
+                passwd=sys.argv[3], db="hbtn_0e_0_usa") as db:
 
-        cur = db.cursor()
-        cur.execute("SELECT * FROM states ORDER BY `id` ASC")
-        allInfo = cur.fetchall()
-        for info in allInfo:
-            print(info)
+            with db.cursor() as cur:
+                cur.execute("SELECT * FROM states ORDER BY `id` ASC")
+                allInfo = cur.fetchall()
+                for info in allInfo:
+                    print(info)
 
     except MySQLdb.Error as e:
         print("MySQL Error[%d]: %s" % (e.args[0], e.args[1]))
-
-    finally:
-        if cur is not None:
-            cur.close()
-        if db is not None:
-            db.close()
+    except Exception as e:
+        print("An error occurred:", e)
