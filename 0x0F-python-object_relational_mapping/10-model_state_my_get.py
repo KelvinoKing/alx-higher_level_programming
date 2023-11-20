8e7a00bea6e3aca9c42cf2a990cdfe678ac8e430
KelvinoKing <kelvinoking@outlook.com>
2023-11-20 08:26:37 +0000

    Updates

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -22,6 +22,7 @@ def get_state_id(user, password, database, state_name):
     engine = create_engine(
             "mysql://{}:{}@localhost:3306/{}".format(user, password, database))
 
+    Base.metadata.bind = engine
     Session = sessionmaker(bind=engine)
 
     try:
