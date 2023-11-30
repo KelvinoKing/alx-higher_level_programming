b661b60e7d4a2daa67f2a79e9080793647444937
KelvinoKing <kelvinoking@outlook.com>
2023-11-30 09:30:26 -0800

    Find peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""function finds the peak of the list
+"""
+
+
+def find_peak(list_of_integers):
+    """Finds peak in an unsorted list
+    """
+
+    if not list_of_integers:
+        return None
+    
+    low, high = 0, len(list_of_integers) - 1
+    
+    while low < high:
+        mid = (low + high) // 2
+
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            high = mid
+
+        else:
+            low = mid + 1
+
+    return list_of_integers[low]
