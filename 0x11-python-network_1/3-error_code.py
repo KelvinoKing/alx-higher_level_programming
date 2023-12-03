54e68612834598b54ef317189cf4bd360fa2e323
KelvinoKing <kelvinoking@outlook.com>
2023-12-03 16:14:12 +0300

    Error code #0

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""import urllib and sys
+"""
+from urllib import request, error
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    try:
+        with request.urlopen(url) as response:
+            body = response.read().decode('utf-8')
+            print(body)
+
+    except error.HTTPError as e:
+        print("Error code: ", e.code)
