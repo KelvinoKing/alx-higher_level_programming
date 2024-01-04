aab41ad087dcf7a99170b9534f6f47d61e4f92f7
KelvinoKing <kelvinoking@outlook.com>
2024-01-04 15:18:51 +0000

    Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,35 +2,35 @@
 
 const request = require('request');
 
-function getTitle (url) {
+function countMoviesWithCharacter (apiUrl, characterId) {
   let count = 0;
 
-  request(url, (error, response, body) => {
+  request(apiUrl, (error, response, body) => {
     if (error) {
       console.error(`Error: ${error.message}`);
     } else if (response.statusCode === 200) {
-      const pageResults = JSON.parse(body);
+      const films = JSON.parse(body).results;
 
-      for (let i = 0; i < pageResults.count; i++) {
-        const myResults = pageResults.results[i];
-        const myChars = myResults.characters;
-        const countValue = myChars.includes('https://swapi-api.alx-tools.com/api/people/18/');
-
-        if (countValue) {
+      films.forEach(film => {
+        const characters = film.characters;
+        if (characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
           count++;
         }
-      }
+      });
+
       console.log(count);
     } else {
-      console.error(`Error: Unexpected status code ${response.statuscode}`);
+      console.error(`Error: Unexpected status code ${response.statusCode}`);
     }
   });
 }
 
 if (process.argv.length !== 3) {
-  console.log('Usage: node script.js <url>');
+  console.log('Usage: node 4-starwars_count.js <API_URL>');
   process.exit(1);
 }
 
-const url = process.argv[2];
-getTitle(url);
+const apiUrl = process.argv[2];
+const characterId = 18;
+
+countMoviesWithCharacter(apiUrl, characterId);
