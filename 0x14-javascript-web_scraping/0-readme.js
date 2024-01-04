51816ad8c20addf5a3f6a54a98dd774e40d4f3fd
KelvinoKing <kelvinoking@outlook.com>
2024-01-04 11:29:51 +0000

    Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,17 +2,17 @@
 
 const fs = require('fs');
 
-if (process.argv.length < 2){
-	console.error('Usage: node 0-readme.js <filepath>');
-	process.exit(1);
+if (process.argv.length < 2) {
+  console.error('Usage: node 0-readme.js <filepath>');
+  process.exit(1);
 }
 
 const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf-8', (err, data) => {
-	if (err) {
-		console.error(err);
-	} else {
-		console.log(data);
-	}
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
 });
