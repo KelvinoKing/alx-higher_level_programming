542b3be6bbce649ee716b916322d18d5cede189f
KelvinoKing <kelvinoking@outlook.com>
2024-01-10 14:36:18 +0000

    Updates

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $.get('https://swapi-api.alx-tools.com/api/people/5/?format=json',
-    function (data, textStatus) {
+    function (data) {
       $('DIV#character').text(data.name);
     });
 });
--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $.get('https://swapi-api.alx-tools.com/api/films/?format=json',
-    function (data, textStatus) {
+    function (data) {
       for (let i = 0; i < data.results.length; i++) {
         const titleVa = data.results[i].title;
         $('UL#list_movies').append('<li>' + titleVa + '</li>');
